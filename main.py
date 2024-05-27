8247560fe9fe692e144ed0da8bca367b1b392331
Matheus Scalabrin <101526350+mcsscalabrin@users.noreply.github.com>
2024-05-27 08:56:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-num1 = int(input())
-num2 = int(input())
+num1 = int(input("n1: "))
+num2 = int(input("n2: "))
 
 soma = num1 + num2
 subt = num1 - num2
 multi = num1 * num2
 divi = num1 / num2
 
-print("soma: " + soma)
-print("subtração: " + subt)
-print("multiplicação: " + multi)
-print("divisão: " + divi)
+print(soma)
+print(subt)
+print(multi)
+print(divi)
