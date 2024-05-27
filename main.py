ec50f8db9e63fa5c8cb635afc76692d8891ff288
Matheus Scalabrin <101526350+mcsscalabrin@users.noreply.github.com>
2024-05-27 08:53:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+
+soma = num1 + num2
+subt = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+
+print("soma: " + soma)
+print("subtração: " + subt)
+print("multiplicação: " + multi)
+print("divisão: " + divi)
